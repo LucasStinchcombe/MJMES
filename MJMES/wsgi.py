d28e6b33b7f993c922d0d97f0e9848ba8dd0024b
LucasStinchcombe <lucasstinchcombe@gmail.com>
2015-03-09 03:17:42 -0400

    Relaunch Heroku

--- MJMES/wsgi.py
+++ MJMES/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os,sys
+import os
 # os.environ.setdefault("DJANDGO_SETTINGS_MODULE", "MJMES.settings")
 os.environ['DJANGO_SETTINGS_MODULE'] = 'MJMES.settings'
-sys.path.append(os.path.dirname(__file__))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
