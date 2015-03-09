29695af48c307c5c2403914344eabcd0bb820fc8
LucasStinchcombe <lucasstinchcombe@gmail.com>
2015-03-09 02:29:19 -0400

    Relaunch Heroku

--- MJMES/settings.py
+++ MJMES/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_markdown',
-    'gunicorn'
+    'gunicorn',
     'blog',
     'archives',
     'about',
