ec09c2940f69641502a3117f08709e94c520faf5
LucasStinchcombe <lucasstinchcombe@gmail.com>
2015-03-09 02:45:04 -0400

    Relaunch Heroku

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.path.dirname(os.path.dirname(__file__))
+    sys.path.append(os.path.dirname(__file__))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MJMES.settings")
     from django.core.management import execute_from_command_line
 
