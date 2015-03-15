550c9727015a6ed58f741ac172eafe2ecdce2f15
LucasStinchcombe <lucasstinchcombe@gmail.com>
2015-03-15 18:43:16 -0400

    Launch Heroku

--- MJMES/settings.py
+++ MJMES/settings.py
@@ -69,6 +69,8 @@ USE_TZ = True
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'mjmesbucket'
+AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 STATIC_URL = 'http://%s.s3-website-us-east-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"),)
